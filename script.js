fb5c11c4dbe83cec6cf6006d98eea01ec6cdc9ae
samerunet <samerunet@gmail.com>
2022-07-27 17:22:59 -0700

    test

---
+++ script.js
@@ -0,0 +1,19 @@
+$(() => {
+	$(window).on("scroll", function () {
+		if ($(window).scrollTop()) {
+			$("nav").addClass("black");
+		} else {
+			$("nav").removeClass("black");
+		}
+	});
+	const $home = () => { 
+		
+	}
+	const $clearDiv = () => {
+		$(".main-content").addClass("hide");
+
+		const $main = $("<div>").addClass(".main-content").appendTo(".container");
+	};
+
+	$("#about").on("click", $clearDiv);
+});
